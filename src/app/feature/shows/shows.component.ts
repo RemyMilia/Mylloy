66742babb58a82d996f40f9b055ea971caa984b8
RemyMilia <milia.remy@gmail.com>
2017-11-12 01:12:22 +0100

    begin show menu

--- src/app/feature/shows/shows.component.ts
+++ src/app/feature/shows/shows.component.ts
@@ -7,9 +7,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShowsComponent implements OnInit {
 
-  constructor() { }
+  public shows: [Show];
+  public ready: boolean;
+
+  constructor() {
+    this.shows =
+      [
+        {
+          date: 'Jan 2018',
+          place: 'L\'Etage',
+          city: 'Paris'
+        },
+        {
+          date: 'Sep 2017',
+          place: 'Parc Floral',
+          city: 'Paris'
+        },
+        {
+          date: 'Jun 2017',
+          place: 'Point Ephemere',
+          city: 'Paris'
+        },
+        {
+          date: 'Apr 2017',
+          place: 'The Hood',
+          city: 'Paris'
+        }
+      ];
+  }
 
   ngOnInit() {
+    $(window).on('load', () => {
+      this.ready = true;
+    });
   }
 
 }
+
+export interface Show {
+  date: any;
+  place: string;
+  city: string;
+}
+
+
+
